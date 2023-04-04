c30d58213ee645b99c03e52089bf706e233da4fe
Parvez <74895218+parvez-tm@users.noreply.github.com>
2023-04-04 13:50:39 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,30 +1,11 @@
-// const { Sequelize } = require('sequelize');
+const {MongoClient} = require('mongodb');
 
-// const sequelize = new Sequelize('student', 'root', '', {
-//   host: 'localhost',
-//   dialect: 'mysql'
-// });
-
-// try {
-//     sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-
-// // sequelize.sync()
-// module.exports = sequelize
-const mongoose = require("mongoose");
-// require("./models/student.model");
-// const student = require("./models/student.model");
-const MONGO_URL =
-  "mongodb+srv://parvez:errorlogin@parvez.hcgqer3.mongodb.net/student?retryWrites=true&w=majority";
-
-mongoose.connect(MONGO_URL);
+const MONGO_URL =   "mongodb+srv://parvez:errorlogin@parvez.hcgqer3.mongodb.net/student?retryWrites=true&w=majority";
 
 
+const client = new MongoClient(MONGO_URL);
 
-mongoose.connection.on("open", () => {
-  console.log("Connection is Ready");
-});
+await client.connect();
+
+// await listDatabases(client);
 
